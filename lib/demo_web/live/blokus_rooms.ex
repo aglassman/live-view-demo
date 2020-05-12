1c430e668d822164b1807f7ed5e012941baf3716
Andy Glassman <aglassman@frame.io>
2020-05-12 10:32:31 -0500

    Added ability to join and create room.

--- lib/demo_web/live/blokus_rooms.ex
+++ lib/demo_web/live/blokus_rooms.ex
@@ -12,30 +12,45 @@ defmodule DemoWeb.BlokusRooms do
 
     if connected?(socket), do: PubSub.subscribe Demo.PubSub, "blokus_events"
 
-    rooms = :global.registered_names() |> Enum.map(&elem(&1,1))
+    rooms = :global.registered_names()
+            |> Enum.map(&elem(&1, 1))
 
-    {:ok, assign(socket, rooms: rooms)}
+    {:ok, assign(socket, rooms: rooms, room_name: "", user_name: "", user_color: "")}
   end
 
- def handle_event("kill", %{"room" => room}, socket) do
+  def handle_event("new-room", %{"room" => %{"name" => name}}, socket) do
+    case GenServer.whereis({:global, {:blokus, name}}) do
+      nil -> Demo.BlokusServer.start_link(name)
+      _ -> true
+    end
+    {:noreply, socket}
+  end
+
+  def handle_event("user-settings", %{"user" => %{"name" => name, "color" => color}}, socket) do
+    {:noreply, assign(socket, user_name: name, user_color: color)}
+  end
+
+  def handle_event("kill-room", %{"room" => room}, socket) do
     case GenServer.whereis({:global, {:blokus, room}}) do
       pid -> GenServer.stop(pid, :kill)
     end
     {:noreply, socket}
   end
 
-  def handle_event(event,assigns,socket) do
+  def handle_event(event, assigns, socket) do
     IO.inspect([:unhandled, event, assigns])
     {:noreply, socket}
   end
 
   def handle_info({:server_start, room}, socket) do
-    rooms = :global.registered_names() |> Enum.map(&elem(&1,1))
+    rooms = :global.registered_names()
+            |> Enum.map(&elem(&1, 1))
     {:noreply, assign(socket, rooms: rooms)}
   end
 
   def handle_info({:server_termination, room}, socket) do
-    rooms = :global.registered_names() |> Enum.map(&elem(&1,1))
+    rooms = :global.registered_names()
+            |> Enum.map(&elem(&1, 1))
     {:noreply, assign(socket, rooms: rooms)}
   end
 
